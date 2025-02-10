f0ae82766ccdb8acb1d8b822ee4b310563811854
abdulshakourr <shakirteck4@gmail.com>
2025-02-10 12:25:07 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8 bg-gray-50">
+        <div className="max-w-5xl mx-auto">
+          <h1>Welcome to the Starter kit</h1>
+          <Button>Start</Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
